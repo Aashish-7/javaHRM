49741f5b08f62587319c7e96f3e1e66c1c86053d
aashish-window <aashishprajapati4183@gmail.com>
2024-06-04 00:28:31 +0530

    Initial commit HRM

---
+++ src/main/java/com/hrm/javaHRM/JavaHrmApplication.java
@@ -0,0 +1,13 @@
+package com.hrm.javaHRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHrmApplication.class, args);
+		System.out.println("TESTING");
+	}
+}
---
+++ src/test/java/com/hrm/javaHRM/JavaHrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.javaHRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
